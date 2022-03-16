c7d58c2ff87d7732afda34449e9ca3119112093d
Kazi Saheb <kzsaheb@gmail.com>
2022-03-16 09:54:57 +0600

    Dynamic User data

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return <div className='App'>
 
-    <Count />
-    <Count />
+    <ExUser />
 
   </div>
 }
 
+// User loading
+function ExUser() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return <div>
+    {
+      users.map(user => <div className='Users' >
+
+        <h1>Name: {user.name}</h1>
+        <p>Email: {user.email}</p>
+        <p>Phone: {user.phone}</p>
+        <p>Address: {user.address.street} , {user.address.city} </p>
+
+      </div>)
+    }
+  </div>
+}
+
+
+
+//Counter componant
 function Count() {
   const [count, setCount] = useState(0)
   const plus = () => setCount(count + 1)
@@ -20,10 +44,6 @@ function Count() {
     <button onClick={down}>Down</button>
   </div>
 }
-
-
-
-
 
 
 export default App;
